497148d803e097e0fbd54fd6db42c9a6c67b93c5
Pholith <vincent22062@orange.fr>
2020-03-21 13:29:02 +0100

    template + start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectDescriptionComponent } from './project-description/project-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectDescriptionComponent
   ],
   imports: [
     BrowserModule,
