2f8429e62b0a5cae3be13fc450485a6ba5244726
Pholith <vincent22062@orange.fr>
2020-03-21 20:31:03 +0100

    gros avancement

--- src/app/project-description/project-description.component.ts
+++ src/app/project-description/project-description.component.ts
@@ -12,4 +12,25 @@ export class ProjectDescriptionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  projects = [
+    {
+      name: "Plants-vs-Zombies",
+      img: "../assets/images/screenPVZ.png",
+      description: "Ce projet est une reproduction du jeu \"Plants VS Zombies\" dans le cadre d'un cours sur la programmation orientée objet. Il a été réalisé en Java.",
+      link: "https://github.com/Pholith/Plants-VS-Zombies"
+    },
+    {
+      name: "Oni-mods",
+      img: "../assets/images/screenONI.png",
+      description: "Ensemble de mods (extensions) sur le jeu Oxygen Not Included de Klei, les mods sont des projets C# qui utilisent la libririe Harmony pour patcher les méthodes du jeu.\
+Ce projet est sûrement celui qui m'a le plus appris en programmation orientée object et en \"hacking\", il n'a pas été facile de comprendre et modifier le jeu qui est très complet et développé par des professionnels.",
+      link: "https://github.com/Pholith/ONI-Mods"
+    },
+    {
+      name: "Site web du chateau de Champs-Sur-Marne",
+      img: "../assets/images/screenChateau.png",
+      description: "Ce site web est mon premier projet tutoré, il s'est déroulé sur toute l'année scolaire 2019-2020 . L'objectif de ce projet est de promouvoir la culture locale et d'attirer de nouveaux visiteurs. Il nous a permis de développer nos compétences en programmation, communication, anglais et encore gestion de projet.",
+      link: "https://github.com/Pholith/Site-Chateau-Champs-sur-Marne"
+    }]
+
 }
