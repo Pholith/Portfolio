6339bb2cf9070cab4bd03e45257519811a78f7e8
Pholith <vincent22062@orange.fr>
2020-03-23 21:49:20 +0100

    Moteur + P5 + minimize assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import * as p5 from 'p5';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +9,124 @@ import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Portfolio';
 
-  @ViewChild('canvas', { static: true })
-  canvas: ElementRef<HTMLCanvasElement>;
+  ngOnInit() {
+    //this.createCanvas();
+  }
 
-  private ctx: CanvasRenderingContext2D;
+/*  private p5;
 
-  ngOnInit(): void {
-    //this.ctx = this.canvas.nativeElement.getContext('2d');
-    //this.ctx.fillStyle = "red";
-    //this.ctx.fillRect(5, 150, 10, 200);
-    //console.log("init");
+
+  private createCanvas() {
+    this.p5 = new p5(this.sketch);
+  }
+
+  private sketch(p: p5) {
+
+    let gameObjects: GameObject[] = [];
+    let toRemoveObjects : GameObject[] = [];
+
+    p.setup = () => {
+      p.frameRate(900);
+      p.createCanvas(p.windowWidth, p.windowHeight).parent('canvas');
+      gameObjects.push(new Square(p.mouseX, p.mouseY, p));
+    };
+
+    p.draw = () => {
+      p.background("#262626")
+
+      gameObjects.push(new Square(p.mouseX, p.mouseY, p));
+
+      // Retire les objects morts de la liste
+      toRemoveObjects.forEach(obj => {
+        let index = gameObjects.indexOf(obj);
+        delete gameObjects[index];
+      });
+      toRemoveObjects = [];
+      
+      // Supprime les gameobjects null
+      gameObjects = gameObjects.filter(function (el) {
+        return el != null;
+      });
+
+      // Update chaque object
+      gameObjects.forEach(gameObject => {
+        gameObject.update();
+        if (gameObject.destroyed()) {
+          toRemoveObjects.push(gameObject);
+        }
+      });
+
+    };
+
+    p.windowResized = () => {
+      p.resizeCanvas(p.windowWidth, p.windowHeight);
+    }
+  }
+
+
+
+}
+
+class GameObject {
+  constructor() {
+  }
+
+  update(): void {
+  }
+
+  destroy(): void {
+    this.destoyed = true;
+  }
+  private destoyed = false;
+  public destroyed() : boolean {
+    return this.destoyed;
   }
 
 }
 
+class Square extends GameObject{
+  constructor(x: number, y: number, p) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.p = p;
+    this.liveTime = 0;
 
+    let random: number = Math.random()
+
+    this.direction = new p5.Vector(5 * (Math.random() - 0.5), 5 * (Math.random() - 0.5));
+
+    this.sizeX = 2 + Math.random() * 2
+    this.sizeY = this.sizeX
+  }
+
+  private x: number;
+  private y: number;
+
+  private direction: p5.Vector;
+  private p;
+
+  private liveTime: number;
+  private liveTimeMax: number = 1000;
+
+  private sizeX: number;
+  private sizeY: number;
+
+  update(): void {
+    this.x += this.direction.x
+    this.y += this.direction.y
+    this.liveTime += this.p.deltaTime;
+
+    if (this.liveTime >= this.liveTimeMax) {
+      this.destroy();
+    }
+    this.draw();
+  }
+
+  draw(): void {
+    this.p.noStroke();
+    this.p.fill(this.p.color("#f47a9d"));
+    this.p.rect(this.x - this.sizeX / 2, this.y - this.sizeY / 2, this.sizeX, this.sizeY);
+  }
+*/
+}
--- src/app/project-description/project-description.component.ts
+++ src/app/project-description/project-description.component.ts
@@ -15,50 +15,51 @@ export class ProjectDescriptionComponent implements OnInit {
   asLink = function(text : String, link : String) : String {
     return "<a target='_blank' href='" + link + "'> "+ text+" </a>";
   }
-  
+  //https://www.esiee.fr/
   projects = [
     {
       name: "Oni-mods",
-      img: "../assets/images/screenONI.png",
+      img: "../assets/images/screenONI.jpg",
       description: "Ensemble de mods (extensions) sur le jeu "+ this.asLink("Oxygen Not Included", "https://www.klei.com/games/oxygen-not-included") + " de Klei (début 2019-actuel), les mods sont des projets .NET C# qui utilisent la librairie "+ this.asLink("Harmony", "https://github.com/pardeike/Harmony/wiki") +" pour patcher les méthodes du jeu. \
-<br/>Ce projet personnel est sûrement celui qui m'a le plus appris en programmation orientée object et en \"hacking\", il n'a pas été facile de comprendre et modifier le jeu qui est très complet et développé par des professionnels. \
-Ce projet m'a aussi appris l'importance de la maintenance du code sur le long terme (devoir rendre les mods accessibles malgré les mise à jour du jeu).",
+<br/>Ce projet personnel est sûrement celui qui m'a le plus appris en programmation orientée objet et en \"hacking\", il n'a pas été facile de comprendre et modifier le code du jeu qui est très complet et développé par des professionnels. \
+Ce projet m'a aussi appris l'importance de la maintenance du code sur le long terme (devoir rendre les mods accessibles malgré les mises à jour du jeu).",
       link: "https://github.com/Pholith/ONI-Mods"
     },
     {
       name: "Plants-vs-Zombies",
       img: "../assets/images/screenPVZ.gif",
       description: "Ce projet est une reproduction du jeu \"Plants VS Zombies\" dans le cadre d'un cours sur la programmation orientée objet (début 2019). \
-Il a été réalisé en en binôme avec le language Java. Ce projet m'a permis d'étendre mes connaissances en poo et à bien répartir les tâches.",
+Il a été réalisé en binôme avec le language Java. Ce projet m'a permis d'étendre mes connaissances en poo et à bien répartir les tâches.",
       link: "https://github.com/Pholith/Plants-VS-Zombies"
     },
     {
       name: "Site web du chateau de Champs-Sur-Marne",
-      img: "../assets/images/screenChateau.png",
-      description: this.asLink("Ce site web", "http://vincent1.heliohost.org/Champs/Site-Chateau-Champs-sur-Marne/html/fr/main.php") +" est mon premier projet tutoré, il s'est déroulé sur toute l'année scolaire 2018-2019. \
-L'objectif de ce projet est de promouvoir la culture locale et d'attirer de nouveaux visiteurs. Il nous a permis de développer nos compétences en programmation, communication, anglais et gestion de projet.",
+      img: "../assets/images/screenChateau.jpg",
+      description: this.asLink("Ce site web", "http://vincent1.heliohost.org/Champs/Site-Chateau-Champs-sur-Marne/html/fr/main.php") +" est mon premier projet tutoré, il s'est déroulé sur toute l'année scolaire 2018-2019 en équipe de 3. \
+L'objectif de ce projet est de promouvoir la culture locale et d'attirer de nouveaux visiteurs. Il nous a permis de développer nos compétences en programmation web, communication, anglais et gestion de projet.",
       link: "https://github.com/Pholith/Site-Chateau-Champs-sur-Marne"
     },
     {
       name: "Pokemon Battle",
       img: "../assets/images/screenPokemon.gif",
       description: "Ce projet est une reproduction du mode de combat du jeu pokémon dans le cadre d'un cours de poo avancée en java (début 2020). \
-La difficulté de ce projet est qu'il nous a été donné à faire en seulement 1mois, et 15 jours ont été requis rien pour préparer son développement...",
+La difficulté de ce projet est qu'il nous a été donné à faire en seulement 1 mois et 15 jours ont été requis rien que pour préparer son développement...",
       link: "https://github.com/Pholith/PokemonBattle"
     },
     {
       name: "Projet D.E.L.I.R.E.",
       img: "../assets/images/screenDELIRE.png",
       description: "Le projet DELIRE (Développement par Equipe de Livrables Informatiques et Réalisation Encadrée) est une simulation d'appel d'offre par l'AP-HP \
-dans la réalisation de leur nouvelle application de gestion des DMP (Dossier médical partagé) dans le cadre de notre cours de gestion de projet (2019-2020). \
-<br/>Ce projet n'a pas été une simulation dans le sens où il nous a donné énormément de travail, et a été très formatteur. J'ai appris grâce à ce projet comment fonctionne le travail en équipe de 6. \
+dans la réalisation de leur nouvelle application de gestion des DMP (Dossier médical partagé) dans le cadre de notre cours de méthodologie et gestion de projet (2019-2020). \
+<br/>Ce projet n'a pas été une simulation dans le sens où il nous a donné énormément de travail et a été très formateur. J'ai appris grâce à ce projet comment fonctionne le travail en équipe de 6. \
 Le site a été réalisé avec Vuejs ainsi que JavaEE avec des api REST.",
-      link: "https://github.com/Pholith/PokemonBattle"
+      link: ""
     },
     {
       name: "Formation Finder",
-      img: "../assets/images/screenFinder.png",
-      description: this.asLink("Ce petit projet", "vincent1.heliohost.org/Finder/") +" est un site dynamique qui utilise les api du gouvernement français pour permettre de rechercher des formations et établissements scolaires (début 2019).",
+      img: "../assets/images/screenFinder.jpg",
+      description: this.asLink("Ce petit projet", "http://vincent1.heliohost.org/Finder/") +" est un site dynamique qui utilise les api du gouvernement français pour permettre de rechercher des formations et établissements scolaires. \
+Il a été réalisé dans le cadre d'un cours de développement web (début 2020).",
       link: "https://github.com/Pholith/ProjetWebOpenData"
     },
     {
